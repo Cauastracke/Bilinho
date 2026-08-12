10f90f2f775660f5a84b1763bcd6656a3b353e1d
Cauã Stracke <cauastracke9@gmail.com>
2026-08-12 16:40:59 -0300

    lógica de faturas melhorada

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -13,35 +13,37 @@ class Enrollment < ApplicationRecord
   after_create :create_invoices
 
   private
-    def create_invoices
-        max_payments.times do |i|
-  
-          invoice_amount = course_total_value/max_payments
-          initial_date = Date.today
-          this_month_due_date = initial_date.change(day: due_date) +i.months
 
-          if this_month_due_date.month == 2 && due_date > 28
-            invoice_due_date = this_month_due_date.end_of_month
-          end
+  def create_invoices
+    today = Date.today
+    due_date_already_passed_this_month = due_date <= today.day
 
-          if due_date == 31
-             invoice_due_date = this_month_due_date.end_of_month
-          end
-
-          if due_date <= initial_date.day
-            invoice_due_date = this_month_due_date + (1).months
-          else 
-            invoice_due_date = this_month_due_date
-          end
-
-          status = "open"
-          invoices.create!(
-            invoice_amount: invoice_amount,
-            invoice_due_date: invoice_due_date,
-            status: status
-          )
-        end
+    if due_date_already_passed_this_month
+      base_month = today + 1.month
+    else
+      base_month = today
     end
 
-end
+    invoice_amount = course_total_value / max_payments
 
+    max_payments.times do |i|
+      current_month = base_month + i.months
+
+      last_day_of_month = current_month.end_of_month.day
+
+      if due_date > last_day_of_month
+        invoice_day = last_day_of_month
+      else
+        invoice_day = due_date
+      end
+
+      invoice_due_date = current_month.change(day: invoice_day)
+
+      invoices.create!(
+        invoice_amount: invoice_amount,
+        invoice_due_date: invoice_due_date,
+        status: "open"
+      )
+    end
+  end
+end
